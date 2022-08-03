3b3e9a2955d9e8e29304a9ada124fae717dff9a8
borcibor <bor.dolenc2@gmail.com>
2022-08-03 15:13:16 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Sirov burek > mesni burek!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
